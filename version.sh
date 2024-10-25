ba31a1f9dfd0eb99982c6bd33430dc9c51c861e6
johnlee <johnlee@miltenyi.com>
2024-10-25 11:37:09 -0400

    update tag finding

--- version.sh
+++ version.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # Get the latest tag
-latest_tag=$(git describe --tags --abbrev=0)
+latest_tag=$(git tag --sort=-v:refname | head -n 1)
 IFS='.' read -r major master develop <<< "$latest_tag"
 
 # Check which part to increment based on branch name
 branch=$(git rev-parse --abbrev-ref HEAD)
-
 if [[ "$branch" == "master" ]]; then
   # Increment master version, reset develop
   master=$((master + 1))
@@ -18,7 +17,6 @@ else
   echo "Not on master or develop branch, version not updated."
   exit 0
 fi
-
 # Create the new version tag
 new_tag="${major}.${master}.${develop}"
 git tag "$new_tag"
